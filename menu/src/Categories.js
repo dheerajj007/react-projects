a6451975fc4370b346601c951e673c05c1139612
fixxtion <dheerajkumarr235@gmail.com>
2023-01-24 07:11:51 +0530

    menu project done!

--- menu/src/Categories.js
+++ menu/src/Categories.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
+import data from "./data";
+import Menu from "./Menu";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, filterItems }) => {
+  console.log(categories);
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Categories;
