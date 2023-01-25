ccaf9eacfac1abcb6400ee472a76d85a356a7dc2
fixxtion <dheerajkumarr235@gmail.com>
2023-01-25 16:13:39 +0530

    lorem epsum generator complete

--- lorem-epsum-generator/src/App.js
+++ lorem-epsum-generator/src/App.js
@@ -1,11 +1,44 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [lines, setLines] = useState(0);
+  const [paras, setParas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newParas = [];
+    let amount = parseInt(lines);
+    if (lines <= 0) {
+      amount = 1;
+    }
+    if (lines > 8) {
+      amount = 8;
+    }
+    setParas(data.slice(0, amount));
+  };
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>TIRED OF BORING LOREM EPSUM</h3>
+      <form className="lorem-form">
+        <label htmlFor="paragraph">Paragraphs: </label>
+        <input
+          type="text"
+          id="lines"
+          name="lines"
+          value={lines}
+          onChange={(e) => setLines(e.target.value)}
+        />
+        <button className="btn" onClick={handleSubmit}>
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {paras.map((para, index) => {
+          return <p key={index}>{para}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
